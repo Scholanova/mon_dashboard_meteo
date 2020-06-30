d8f6f401cdd7aa0cd5a32c3305eccf2852ad2a9b
elsene <magenelec@gmail.com>
2020-06-30 14:41:09 +0200

    init projet hello word + architecture

---
+++ src/main/java/com/schola/mydashboardweather/MydashboardweatherApplication.java
@@ -0,0 +1,14 @@
+package com.schola.mydashboardweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydashboardweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydashboardweatherApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/schola/mydashboardweather/MydashboardweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schola.mydashboardweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydashboardweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
