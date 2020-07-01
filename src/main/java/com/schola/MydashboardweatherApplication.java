2423ab9ea822a1ef8c54ffee38b3e5b6117af7d9
elsene <magenelec@gmail.com>
2020-07-01 15:49:50 +0200

    add config bdd migration in main app

--- src/main/java/com/schola/MydashboardweatherApplication.java
+++ src/main/java/com/schola/MydashboardweatherApplication.java
@@ -4,13 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jmx.export.MBeanExporter;
 
 @SpringBootApplication
-public class MydashboardweatherApplication{
+public class MydashboardweatherApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MydashboardweatherApplication.class, args);
-	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(MydashboardweatherApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydashboardweatherApplication.class, args);
+    }
+    @Bean
+    public MBeanExporter exporter(){
+        final  MBeanExporter exporter = new MBeanExporter();
+        exporter.setAutodetect(true);
+        exporter.setExcludedBeans("datasource");
+        return exporter;
+    }
 
 }
