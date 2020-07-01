39564dc0de89b8abb58afb6465adb58cb1804671
Daouda Fofana <fofanadaouda93230@gmail.com>
2020-07-01 16:12:22 +0200

    creation du model Alert: DONE

---
+++ src/main/java/com/schola/mydashboardweather/models/Alert.java
@@ -0,0 +1,35 @@
+package com.schola.mydashboardweather.models;
+
+public class Alert {
+    private Integer id;
+    private String time;
+    private String location;
+    private Integer repeat;
+
+    public Alert() {
+    }
+
+    public Alert(Integer id, String time, String location, Integer repeat) {
+        this.id = id;
+        this.time = time;
+        this.location = location;
+        this.repeat = repeat;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Integer getRepeat() {
+        return repeat;
+    }
+    
+}
