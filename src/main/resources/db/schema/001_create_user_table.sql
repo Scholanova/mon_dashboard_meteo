3b64fd7a90867766de56c9bc6802d818fd0ab4c3
Gomes Nicolas <gomes.nicolas@outlook.fr>
2020-07-02 15:52:57 +0200

    Add_Location_and_User_Script

--- src/main/resources/db/schema/001_create_user_table.sql
+++ src/main/resources/db/schema/001_create_user_table.sql
@@ -2,9 +2,9 @@
 
 --changeset scholanova:1
 CREATE TABLE IF NOT EXISTS public.USER (
-  id        NUMERIC NOT NULL,
-  lastName	VARCHAR(255),
-  firstName	VARCHAR(255),
+  id        SERIAL NOT NULL,
+  lastname	VARCHAR(255),
+  firstname	VARCHAR(255),
   email     VARCHAR(255),
   password  VARCHAR(255),
   PRIMARY KEY (id)
