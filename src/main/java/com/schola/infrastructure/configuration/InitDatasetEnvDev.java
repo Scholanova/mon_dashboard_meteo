d3be2a1753b1bd762960c747752c348e3f742deb
elsene <magenelec@gmail.com>
2020-07-02 10:01:36 +0200

    fix error no host recognized for migration

--- src/main/java/com/schola/infrastructure/configuration/InitDatasetEnvDev.java
+++ src/main/java/com/schola/infrastructure/configuration/InitDatasetEnvDev.java
@@ -1,8 +1,37 @@
 package com.schola.infrastructure.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Conditional(value = {MockProfileCondition.class})
 public class InitDatasetEnvDev {
+    @Autowired
+    protected DataSource dataSource;;
+
+    @Autowired
+    protected Environment env;
+
+
+    @PostConstruct
+    public void initDataSet() throws SQLException {
+
+        try
+            (Connection connection = dataSource.getConnection()){
+                connection.getSchema();
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("/db/00_drop.sql"));
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("/db/01_create.sql"));
+
+        }
+
+        }
+
 }
---
+++ src/main/resources/db/01_create.sql
@@ -0,0 +1,5 @@
+create table USER(
+id varchar(20) not null,
+nom varchar(50) null;
+constraint PK_ID_USER PRIMARY KEY (id)
+);
