781b98112ff6cf0e0b18923f0bef82ea11f53570
Daouda Fofana <fofanadaouda93230@gmail.com>
2020-07-03 16:28:22 +0200

    add controller AlertController

--- src/main/java/com/schola/controller/alert/AlertController.java
+++ src/main/java/com/schola/controller/alert/AlertController.java
@@ -3,6 +3,7 @@ package com.schola.controller.alert;
 import com.schola.repository.alert.AlertRepository;
 import com.schola.entity.alert.Alert;
 import com.schola.repository.alert.AlertRepository;
+import com.schola.services.alert.AlertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class AlertController {
 
+    private AlertService alertService;
+
+    public AlertController(AlertService alertService) {
+        this.alertService = alertService;
+    }
+
+    @GetMapping("/")
+    public String listAlert(Model model) {
+        List<Alert> alerts = alertService.listAll();
+
+        model.addAttribute("alerts", alerts);
+        return "alert-list";
+    }
 
 
 }
