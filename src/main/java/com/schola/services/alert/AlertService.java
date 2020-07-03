fb8db30d056de84c42440f17c661b0c0944d145c
Daouda Fofana <fofanadaouda93230@gmail.com>
2020-07-03 16:29:08 +0200

    add service AlertService

--- src/main/java/com/schola/services/alert/AlertService.java
+++ src/main/java/com/schola/services/alert/AlertService.java
@@ -1,4 +1,19 @@
 package com.schola.services.alert;
 
+import com.schola.entity.alert.Alert;
+import com.schola.repository.alert.AlertRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AlertService {
+    private AlertRepository alertRepository;
+
+    public AlertService(AlertRepository alertRepository) {
+        this.alertRepository = alertRepository;
+    }
+    public List<Alert> listAll() {
+        return alertRepository.listAll();
+    }
 }
