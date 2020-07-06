0fd8261ceb6a16e5bac1a9e94c4a5f08d2d5d5a4
elsene <magenelec@gmail.com>
2020-07-07 01:56:37 +0200

    config change for encoder password

--- src/main/java/com/schola/config/SecurityConfiguration.java
+++ src/main/java/com/schola/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.schola.config;
 
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,10 +12,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 	private final UserDetailsService userDetailsService;
 
 	@Autowired
@@ -31,13 +40,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.anyRequest().permitAll()
 				.and()
-				.formLogin().loginPage("/login").defaultSuccessUrl("/").failureUrl("/error")
-				.usernameParameter("email").passwordParameter("password")
+				.formLogin().loginPage("/login").defaultSuccessUrl("/main", true).failureUrl("/index-error")
+				.usernameParameter("username").passwordParameter("password")
 				.and()
 				.logout().invalidateHttpSession(true)
 				.logoutUrl("/logout")
@@ -47,4 +57,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 				.sessionManagement().maximumSessions(1).expiredUrl("/login");
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/");
+	}
+
+
 }
