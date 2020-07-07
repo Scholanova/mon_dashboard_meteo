40b7c5fda15ef1a03c8a302e0a7aa14f5882293e
elsene <magenelec@gmail.com>
2020-07-07 04:01:42 +0200

    fix deploiement for master probléme other table

--- src/main/resources/db/schema/001_create_userlocation_table.sql
+++ src/main/resources/db/schema/001_create_userlocation_table.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS public.USERLOCATION (
   user_id       INTEGER NOT NULL,
   location_id   INTEGER NOT NULL,
   PRIMARY KEY (user_id, location_id),
-  FOREIGN KEY (user_id) REFERENCES public.USER(id),
+  FOREIGN KEY (user_id) REFERENCES public.USER(ID_USER),
   FOREIGN KEY (location_id) REFERENCES public.LOCATION(id)
 );
