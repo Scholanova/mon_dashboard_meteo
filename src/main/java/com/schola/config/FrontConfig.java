c0a63fa5679a25f193faf1c88043c429832ec4a3
FrancoisGodin <francois.h.godin@gmail.com>
2020-07-08 16:02:19 +0200

    ADD redirection on GET /favorites-locations if user is null

--- src/main/java/com/schola/config/FrontConfig.java
+++ src/main/java/com/schola/config/FrontConfig.java
@@ -18,7 +18,7 @@ public class FrontConfig  extends WebMvcConfigurerAdapter {
        // registry.addViewController("/login").setViewName("authentification/login");
         registry.addViewController("/register").setViewName("authentification/register");
         registry.addViewController("/main").setViewName("main/main");
-        registry.addViewController("/location").setViewName("location/location-list");
+        registry.addViewController("/favorites-locations").setViewName("location/location-list");
         registry.addViewController("/weatherlocation").setViewName("main/weatherlocation");
     }
 
