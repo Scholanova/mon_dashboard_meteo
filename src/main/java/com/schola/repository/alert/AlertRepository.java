934c44dc4ebc42ba0f1ea9fff83a8d669ca211c0
Daouda Fofana <fofanadaouda93230@gmail.com>
2020-07-08 09:50:05 +0200

    correction after elimane review

--- src/main/java/com/schola/repository/alert/AlertRepository.java
+++ src/main/java/com/schola/repository/alert/AlertRepository.java
@@ -1,33 +1,10 @@
 package com.schola.repository.alert;
 
 import com.schola.entity.alert.Alert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Repository
-public class AlertRepository {
-
-    @Autowired
-    private NamedParameterJdbcTemplate jdbcTemplate;
-
-    public List<Alert> listAll() {
-        String query = "SELECT ID as id, " +
-                    "date AS date " +
-                    "days AS days " +
-                    "hour As hour " +
-                "FROM Alert";
-
-        Map<String, Object> parameters = new HashMap<>();
-
-        return jdbcTemplate.query(query,
-                parameters,
-                new BeanPropertyRowMapper<>(Alert.class));
-    }
-
+public interface AlertRepository extends JpaRepository<Alert, Integer> {
 }
