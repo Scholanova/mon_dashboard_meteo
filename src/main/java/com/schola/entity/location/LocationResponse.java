73eb8ca901dd638b46d6b0239f5d9ac8cbc4cf96
Gomes Nicolas <gomes.nicolas@outlook.fr>
2020-07-08 12:15:48 +0200

    Add_2_API_and_models_and_Front

---
+++ src/main/java/com/schola/entity/location/LocationResponse.java
@@ -0,0 +1,55 @@
+package com.schola.entity.location;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class LocationResponse {
+
+    private String nom;
+    private String code;
+    private String codeDepartement;
+    private String codeRegion;
+    private List<String> codesPostaux;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCodeDepartement() {
+        return codeDepartement;
+    }
+
+    public void setCodeDepartement(String codeDepartement) {
+        this.codeDepartement = codeDepartement;
+    }
+
+    public String getCodeRegion() {
+        return codeRegion;
+    }
+
+    public void setCodeRegion(String codeRegion) {
+        this.codeRegion = codeRegion;
+    }
+
+    public List<String> getCodesPostaux() {
+        return codesPostaux;
+    }
+
+    public void setCodesPostaux(List<String> codesPostaux) {
+        this.codesPostaux = codesPostaux;
+    }
+}
